99882886e0ec7f65cf3f37615bef83faaadb1ed8
Marko Bogdanović <marko_bogdanovic@outlook.com>
2023-04-03 09:52:48 +0200

    Second commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'first-project';
+
+  constructor(private router:Router) { }
+
+  public pages2= [{
+    pageTitle: 'Početna',
+    link: 'home',
+    subPages:[]
+  },
+  {
+    pageTitle: 'O nama',
+    link: 'about',
+    subPages:[
+      {
+        pageTitle: "Naši projekti",
+        link:"about"
+      }
+    ]
+  }]
+
+  pageClick(item){
+    this.router.navigate(["/"+item]);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PageModule } from './page/page.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PageModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
